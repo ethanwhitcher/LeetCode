188bfbe57a743fce0f6fa222d0e579a04cfa48cc
ethanwhitcher <ewhitcher123@gmail.com>
2023-05-24 15:45:56 -0400

    added koko eating bananas

---
+++ binarysearch/kokoEatingBananas.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def feasible(speed) -> bool:
+            return sum(math.ceil(pile/speed) for pile in piles) <= h
+        
+        left, right = 1, max(piles)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
